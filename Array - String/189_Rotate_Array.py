4f82f775abed84c3c2629bccdade2aca44b58d2c
khusrav13 <khusravvvb99@gmail.com>
2025-04-20 15:09:55 +0900

    189_update

--- Array - String/189_Rotate_Array.py
+++ Array - String/189_Rotate_Array.py
@@ -1,10 +1,27 @@
-def swap_ends_inplace(arr):
-    if len(arr) > 1:
-        arr[0], arr[-1] = arr[-1], arr[0]
-    print(f"After swapping: {arr}")
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
 
-my_list = [10,20,30,40]
-swap_ends_inplace(my_list)
+        # Reverse the entire array
+        self.reverse(nums, 0, n - 1)
 
-another_list = [5]
-swap_ends_inplace(another_list)
+        # Reverse the first k elements
+        self.reverse(nums, 0, k - 1)
+
+        # Reverse the remaining n-k elements
+        self.reverse(nums, k, n - 1)
+
+    def reverse(self, nums, start, end):
+        """
+        Helper function to reverse a portion of the array in-place.
+        """
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 
